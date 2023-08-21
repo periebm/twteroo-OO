7b7a46be1f8fbca9d41687819eaa8750cc0d759d
Pericles <periclesebmendes@gmail.com>
2023-08-20 21:16:08 -0300

    feat: user entity

---
+++ entities/user.entity.ts
@@ -0,0 +1,9 @@
+export class User {
+  private name: string;
+  private email: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,3 +10,74 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+/*
+const tweets = []   // { username: "bobesponja", tweet: "Oi tudo bom?" }
+const users = []    // { username: "bobesponja", avatar: "www.imagem.com" }
+
+// Funções (endpoints)
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body
+
+    if (!username || typeof username !== "string" || !avatar || typeof avatar !== "string") {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
+    users.push({ username, avatar })
+    res.status(201).send("OK")
+})
+
+app.post("/tweets", (req, res) => {
+    const { tweet } = req.body
+    const { user } = req.headers
+
+    if (!user || typeof user !== "string" || !tweet || typeof tweet !== "string") {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
+    // find => retornar undefined se não achar OU o objeto do usuário se achar
+    const userExists = users.find((u) => u.username === user)
+
+    if (!userExists) return res.status(401).send("UNAUTHORIZED")
+
+    tweets.push({ username: user, tweet })
+    res.status(201).send("OK")
+})
+
+app.get("/tweets", (req, res) => {
+    const page = Number(req.query.page)
+
+    if (req.query.page && (isNaN(page) || page < 1)) {
+        return res.status(400).send("Informe uma página válida!")
+    }
+
+    const completeTweets = tweets.map((tweet) => {
+        const user = users.find((u) => u.username === tweet.username)
+        return { ...tweet, avatar: user.avatar }
+    })
+
+    if (page) {
+        const limit = 10
+        const start = (page - 1) * limit
+        const end = page * limit
+
+        return res.send(completeTweets.slice(start, end))
+    }
+
+    res.send(completeTweets.slice(-10))
+})
+
+app.get("/tweets/:username", (req, res) => {
+    const { username } = req.params
+
+    const filteredTweets = tweets
+        .filter((tweet) => tweet.username === username)
+        .map((tweet) => {
+            const user = users.find((u) => u.username === tweet.username)
+            return { ...tweet, avatar: user.avatar }
+        })
+
+    res.send(filteredTweets)
+})
+
+*/
